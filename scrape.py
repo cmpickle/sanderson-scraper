5c4d67faaa506b0230708286645c95d43509a623
Ryan Dufrene <rdufrene92@gmail.com>
2020-10-05 02:31:04 -0400

    Made this look like a person who has heard of software engineering has smelled this, now 100% sure is work but it seems like it is good.

--- scrape.py
+++ scrape.py
@@ -1,3 +1,4 @@
+import sys
 import requests
 import json
 import datetime
@@ -15,20 +16,45 @@ project_path = os.path.dirname(os.path.abspath(__file__))
 
 # set file names
 last_update_file = project_path + "/lastupdate"
+failed_numbers_file_path = project_path + "/failednumbers"
 config_file = project_path + "/config.json"
 
 # specify the url
 scrape_url = 'https://brandonsanderson.com/'
 
-def send_texts(message, config_file):
+# what type is of parser to use
+soup_parser_type = 'html.parser'
+
+class SandersonStatus:
+    work_status_map = {}
+    def add_work(self, work_name, work_progress):
+        self.work_status_map[work_name] = work_progress
+
+    def get_work_status_map(self) -> dict:
+        return self.work_status_map
+
+    def set_work_status_map(self, new_work_status_map):
+        self.work_status_map = new_work_status_map
+
+    def __eq__(self, other): 
+        if not isinstance(other, SandersonStatus):
+            # don't attempt to compare against unrelated types
+            return False
+        return self.work_status_map == other.get_work_status_map()
+
+    #TODO: add a json encoder and decoder for this object and use that for saving
+
+def send_texts(message, config_file) -> dict:
+    success = False
+    failed_numbers = []
     headers = {
         'Content-Type': 'application/json',
     }
     with open(config_file) as json_file:
         config = json.load(json_file)
         url_base = config['url']
+        failed_numbers_file = open(failed_numbers_file_path, "w")
         for user in config['all_users']:
-            print(user)
             payload = {
                 'number': user['number'],
                 'message': message,
@@ -37,72 +63,104 @@ def send_texts(message, config_file):
             text_url = url_base + user['location_path']
             response = requests.post(text_url, headers=headers, data=json.dumps(payload))
             response.raise_for_status()
-            print(user['number'], " - ", response.json())
+            response_log_str = str(user['number']) + " - " + response.text
+            if response.json()['success'] is False:
+                failed_numbers.append(response_log_str)
+                failed_numbers_file.write(response_log_str + "\n")
+            else:
+                success = True
+            # print for logging
+            print(response_log_str)
+        failed_numbers_file.close()
+    print()
+    if not success:
+        raise RuntimeError("Could not send any text messages, please check the config.json file and textbelt setup")
+    return failed_numbers
+
+def get_values():
+    # class to hold the current status information
+    current_status = SandersonStatus()
+
+    try:
+        # query the website and return the html to the variable 'page'
+        page = requests.get(scrape_url)
+        # parse the html using beautiful soup and store in variable `soup`
+        soup = BeautifulSoup(page.text, soup_parser_type)        
+        # get list of book titles and check name (NOTE: this is where to update if the website elements' format is updated again)
+        name_boxs = soup.find_all(attrs={'class': name_boxs_html_name})
+        if name_boxs is None:
+            raise RuntimeError("Could not find name box with html name " + name_boxs_html_name)
+        # extract the status from the name box and add them to the return object
+        for name in name_boxs:
+            status = name.find(attrs={'class': status_boxs_html_name}).extract()
+            if status is None:
+                raise RuntimeError("Could not find status boxs with html name " + status_boxs_html_name + " for the work " + name.text.strip())
+            current_status.add_work(name.text.strip(), status.text.strip())
+    except RuntimeError as err: 
+        print("Error: " + str(err))
+        traceback.print_exc() 
+        sys.exit()
+    except Exception as err: 
+        print("Error: " + str(err))
+        traceback.print_exc() 
+        sys.exit()
+
+    return current_status
+
+def is_update(current_status):
+    try:
+        saved_file = open(last_update_file, "r")
+        saved_work_status_map_json = saved_file.read()
+        if saved_work_status_map_json == "":
+            saved_file.close()
+            return True
+        last_update_work_status_map = json.loads(saved_work_status_map_json)
+        saved_file.close()
+            
+        return not current_status.get_work_status_map() == last_update_work_status_map
+    except IOError:
+        # this usually means it cannot find the file so this will be an update
+        return True
+    except Exception as err: 
+        print("Error: " + str(err))
+        traceback.print_exc() 
+        sys.exit()
+
+
+def create_update_message(update):
+    # handle the data scrapped
+    output = "Brandon Sanderson Status Update:"
+    # create data string to be sent
+    for work in status.get_work_status_map():
+        output += ("\n" + work + " - " +  status.get_work_status_map()[work])
+    return output
+    
+def save_update(update):
+    save_file = open(last_update_file, "w")
+    save_file.write(json.dumps(update.get_work_status_map()))
 
 # output for logging            
 print("------------------------------------------------------------------------")
 print(datetime.datetime.now())
+print()
 
-try:
-    # query the website and return the html to the variable 'page'
-    page = requests.get(scrape_url)
+status = get_values()
 
-    # parse the html using beautiful soup and store in variable `soup`
-    soup = BeautifulSoup(page.text, 'html.parser')
-
-    # get progress bar div to limit the page and check
-    progress_bar = soup.find(attrs={'class': progress_bar_html_name})
-    if progress_bar is None:
-        raise RuntimeError("Could not find progress bar with html name " + progress_bar_html_name)
-    print("progress_bar:")
-    print(progress_bar.prettify())
-
-    status_boxs = []
-    
-    # get list of book titles and check name
-    name_boxs = progress_bar.find_all(attrs={'class': name_boxs_html_name})
-    if name_boxs is None:
-        raise RuntimeError("Could not find name box with html name " + name_boxs_html_name)
-    # extract the status from the name box for better formatting later
-    for name in name_boxs:
-        status = name.find(attrs={'class': status_boxs_html_name})
-        status_boxs.append(status.extract())
-
-    if len(status_boxs) < 1:
-        raise RuntimeError("Could not find status boxs with html name " + status_boxs_html_name)
-
-    # if problem with scrapping
-    if len(name_boxs) != len(status_boxs) or len(name_boxs) == 0 or len(status_boxs) == 0: 
-        raise RuntimeError("NUmber of Titles and Status arrays don't match.")
-
-    # handle the data scrapped
-    else:
-        final_output = "Brandon Sanderson Status Update:"
-        index = 0
-        # create data string to be sent
-        for name in name_boxs:
-            final_output += ("\n" + name.text.strip() + " - " + status_boxs[index].text)
-            index += 1
-        try:
-            print()
-            save_file = open(last_update_file, "r")
-            last_update = save_file.read()
-            save_file.close()
-        except IOError:
-            last_update = ""
-
-        # new update so update and send message(s)
-        if last_update != final_output:
-            print(final_output.strip())
-            print()
-            send_texts(final_output, config_file)
-            save_file = open(last_update_file, "w")
-            save_file.write(final_output)
-            save_file.close()
-        else:
-            print("No change to the progress")
-except RuntimeError as err: 
-    print("Error: " + str(err))
-except Exception as err: 
-    print("Error: " + str(err))
-    traceback.print_exc() 
+if is_update(status):
+    update_message = create_update_message(status)
+    # output for logging
+    print(update_message.strip())
+    print()
+    # send text messages
+    try:
+        failed_messages = send_texts(update_message, config_file)
+    except FileNotFoundError:
+        print("Error: Could not find the config file please make sure it is created and correct")
+        sys.exit()
+    except RuntimeError as err:
+        print("Error: " + str(err))
+        sys.exit()
+    # save the new update
+    save_update(status)
+else:
+    print("No change to the progress")
